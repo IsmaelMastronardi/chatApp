017704130acc1d2658a4e1687054c71ed5715fc5
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-07-12 20:52:00 -0300

    add chats controller - update channel subscribe method

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,24 @@
+class ChatsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @chats = current_user.chats
+  end
+
+  def create
+    @chat = Chat.between(params[:user1_id], params[:user2_id]).first_or_create!(chat_params)
+    redirect_to chat_path(@chat)
+  end
+
+  def show
+    @chat = Chat.find(params[:id])
+    @messages = @chat.messages
+    @message = Message.new
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:user1_id, :user2_id)
+  end
+end

7f703573b66b1a0e0644e67ad1e19b0d48dffec1
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-07-12 20:52:35 -0300

    update chat_channel.js to handle 1to1 chats

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,19 +1,24 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("ChatChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log('CONNECTED')
-  },
+document.addEventListener('DOMContentLoaded', () => {
+  const chatElement = document.getElementById('chat-id');
+  if (chatElement) {
+    const chatId = chatElement.getAttribute('data-chat-id')
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-    console.log('DISCONNECTED')
-  },
-
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    const messages = document.getElementById('messages')
-    messages.insertAdjacentHTML('beforeend', data.message)
+    consumer.subscriptions.create({ channel: "ChatChannel", chat_id: chatId }, {
+      connected() {
+        // Called when the subscription is ready for use on the server
+        console.log(`Connected to chat channel ${chatId}!`)
+      },
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+        console.log(`Disconnected from chat channel ${chatId}!`)
+      },
+      received(data) {
+        // Called when there's incoming data on the websocket for this channel
+        const messages = document.getElementById('messages')
+        messages.insertAdjacentHTML('beforeend', data.message)
+      }
+    })
   }
 });

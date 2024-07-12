916115eed23238cb201f0bc494aee9921da2eec9
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-07-12 12:50:29 -0300

    fix bug on create method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,24 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
   def index
-  end
-
-  def show
+    @messages = Message.all
+    @message = Message.new
   end
 
   def create
+    @message = current_user.messages.build(message_params)
+    if @message.save
+      ActionCable.server.broadcast 'chat_channel', {message: render_message(@message)}
+    end
   end
+
+  private
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def render_message(message)
+    render(partial: 'message', locals: { message: message })
+  end
+
 end

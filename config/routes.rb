47eebe87c876fc9c0b593c4198c1215456676bfb
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2024-07-12 20:53:52 -0300

    update routes to add chats

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :messages, only: [:index, :create]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :chats, only: [:index, :create, :show] do
+    resources :messages, only: [:create]
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  root "messages#index"
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root "chats#index"
 end
